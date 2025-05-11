1ec42a6631343d3a1b560a80e2954e35a1d31cc6
Hugo Ramos <hu.ramos@duocuc.cl>
2025-05-10 23:30:34 -0400

    "mejoras para comunicacion front-back"

--- target/generated-sources/archetype/target/test-classes/projects/basic/project/basic/src/main/java/it/pkg/repository/TopicRepository.java
+++
@@ -1,8 +0,0 @@
-package it.pkg.repository;
-
-import it.pkg.model.Topic;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TopicRepository extends JpaRepository<Topic, Long> {
-    // Métodos personalizados si los necesitas
-}
